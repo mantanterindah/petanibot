ba3970c1ca2de10fb2b06a813ca8bff6f09cc93b
mantanterindah <tenuz99@gmail.com>
2022-09-07 20:01:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const { Client, GatewayIntentBits , Collection} = require('discord.js');
+const {REST} = require('@discordjs/rest');
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+const dotenv = require('dotenv');
+
+//dotenv.config();
+const fs = require('fs')
+//const { token } = require('./config.token');
+const { clientId, guildId, token } = require('./config.json');
+//dotenv.config();
+//const clientId = process.env.clientId
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.on('ready', () => {
+    const Games = [`Ragnarok Retro`, `Age of Ragnarok`, 'Dragon Nest Return']
+    setInterval(() => { client.user.setActivity(`${Games[Math.floor(Math.random() * Games.length)] }`) }, 30000)
+});
+
+// Command Handling
+client.commands = new Collection()
+
+fs.readdirSync('./Events').forEach(dirs => {
+  const EventFiles = fs.readdirSync(`./Events/${dirs}`)
+    .filter(file =>
+      file.endsWith('.js')
+           )
+    
+  for (const file of EventFiles) {
+    const EventName = file.split(".")[0]
+    const Event = require(`./Events/${dirs}/${file}`)
+    client.on(EventName, Event.bind(null, client))
+  }
+})
+
+const commands = [];
+
+fs.readdirSync('./Commands').forEach(dirs => {
+  const CommandFiles = fs.readdirSync(`./Commands/${dirs}`)
+    .filter(file =>
+      file.endsWith('.js')
+           )
+    
+  for (const file of CommandFiles) {
+    const command = require(`./Commands/${dirs}/${file}`)
+    commands.push(command.data.toJSON())
+    client.commands.set(command.data.name, command)
+  }
+})
+
+const rest = new REST({version: '10'}).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+    
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+        body: commands
+      },
+    );
+    
+    console.log('Successfully reloaded application (/) commands.');
+  
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+  
+  console.log(`Client ready`);
+
+//});
+
+client.login(token).catch(console.error)
